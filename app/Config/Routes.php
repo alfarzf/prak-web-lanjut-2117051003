ded04084d1cdf60327992f695085c2067916a3e6
Alfa Rizki <alfarizkif.29@gmail.com>
2023-09-11 20:49:31 +0700

    nambah page profile

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,11 @@
 <?php
-
+// use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/profile', 'Home::profile');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+// $routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
---
+++ app/Views/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Profile</title>
+    <link rel="stylesheet" href="<?php echo base_url('./css/style.css'); ?>">
+</head>
+<body>
+<div class="center">
+        <img class ='img' src="https://cdn.discordapp.com/attachments/857891537986781186/1129757556336754688/961783.jpg" alt="Ini Gambar" height=175px width=175px>
+        <p class="p"><?= $nama ?></p>
+        <p class="p"><?= $kelas ?></p>
+        <p class="p"><?= $npm ?></p>
+</div>
+</body>
+</html>
